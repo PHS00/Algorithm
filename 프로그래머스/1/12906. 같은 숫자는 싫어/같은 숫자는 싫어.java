52b1fdb984875b88536edacf92d78b808d7fadda
박현수 <88030920+PHS00@users.noreply.github.com>
2023-11-08 17:17:38 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 30.37 ms, Memory: 109 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+    public Stack<Integer> solution(int []arr) {
+        Stack<Integer> answer = new Stack<>();
+
+        answer.push(arr[0]);
+        for(int i = 1; i < arr.length; i++){
+            if(answer.peek() == arr[i]){
+                continue;
+            }
+            answer.push(arr[i]);
+        }
+
+        return answer;
+    }
+}
