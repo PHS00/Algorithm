1c0df6630215ade2cb8ad63415a323b806631099
박현수 <88030920+PHS00@users.noreply.github.com>
2023-12-07 14:47:46 +0900

    [Silver III] Title: 모든 순열, Time: 2784 ms, Memory: 73240 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+// 모든 순열
+public class Main {
+    static public boolean[] visit;
+    static public int N;
+    static public int[] arr;
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        N = scanner.nextInt();
+        visit = new boolean[N+1];
+        arr = new int[N];
+        permutation(0);
+
+    }
+    private static void permutation(int cnt){
+        if(cnt == N){
+            for(int i = 0; i < arr.length; i++){
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+        for(int i = 1; i <= N; i++){
+            if(!visit[i]){
+                visit[i] = true;
+                arr[cnt] = i;
+                permutation(cnt+1);
+                visit[i] = false;
+            }
+        }
+    }
+}
